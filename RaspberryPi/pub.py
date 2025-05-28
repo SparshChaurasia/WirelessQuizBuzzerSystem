84e4de2930ef87962e33b8dba190297bc6333074
SparshChaurasia <sparshchaurasia0021@gmail.com>
2025-05-28 23:43:28 +0530

    updated files

---
+++ RaspberryPi/pub.py
@@ -0,0 +1,36 @@
+import paho.mqtt.client as mqtt
+
+MQTT_HOST = "192.168.1.25"
+MQTT_PORT = 1883
+MQTT_KEEPALIVE_INTERVAL = 5
+MQTT_TOPIC = "device/reset"
+MQTT_MSG = "Hello MQTT"
+
+# Define on_connect event Handler
+def on_connect(mosq, obj, rc):
+    print("Connected to MQTT Broker")
+
+
+# Define on_publish event Handler
+def on_publish(client, userdata, mid):
+    print("Message Published...")
+
+
+# Initiate MQTT Client
+mqttc = mqtt.Client()
+
+# Register Event Handlers
+mqttc.on_publish = on_publish
+mqttc.on_connect = on_connect
+
+# Connect with MQTT Broker
+mqttc.connect(MQTT_HOST, MQTT_PORT, MQTT_KEEPALIVE_INTERVAL)
+
+while True:
+    # Check if button is pressed on the board
+
+    # If pressed, publish message to MQTT Topic
+    mqttc.publish(MQTT_TOPIC, MQTT_MSG)
+
+# Disconnect from MQTT_Broker
+mqttc.disconnect()
